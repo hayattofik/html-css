f96ad135e8c1c2900634685b0c4ae2b4a72cab51
hayattofik <96552730+hayattofik@users.noreply.github.com>
2022-01-29 09:09:22 +0300

    Delete ethiop directory

--- ethiop/src/main/java/com/hagere/ethiop/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hagere.ethiop.repositories;
-import com.hagere.ethiop.Entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-     public User findByUsername(String username);
-}
